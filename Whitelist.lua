24ad18ee6cc74631951123120b80ef57420a6a68
BRUHbRuh965 <92668921+BRUHbRuh965@users.noreply.github.com>
2021-10-28 14:45:03 +0700

    Add files via upload

---
+++ Whitelist.lua
@@ -0,0 +1,7 @@
+local key = _G.Key
+local check = "https://sucksuckhub.000webhostapp.com/Check.php?key=" .. key
+if game:HttpGet(check) == "Whitelisted" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BRUHbRuh965/NOCOPY/main/BloxFruit.lua"))()
+else
+game.Players.LocalPlayer:Kick("Hwidไม่ถูกรันหาพ่อ Hwidไม่ถูกรันหาพ่อ Hwidไม่ถูกรันหาพ่อ Hwidไม่ถูกรันหาพ่อ Hwidไม่ถูกรันหาพ่อ Hwidไม่ถูกรันหาพ่อ Hwidไม่ถูกรันหาพ่อ Hwidไม่ถูกรันหาพ่อ Hwidไม่ถูกรันหาพ่อ Hwidไม่ถูกรันหาพ่อ Hwidไม่ถูกรันหาพ่อ")
+end
